d09ad8be0ff48ff0d418f8eb9185570d9d6f6aaf
Agatha North <rouborn@gmail.com>
2020-07-06 16:32:33 -0700

    fiddling with api endpoint

--- rally-capstone-frontend/src/config.js
+++ rally-capstone-frontend/src/config.js
@@ -1,3 +1,3 @@
-export const API_ENDPOINT = process.env.API_ENDPOINT || 'http://localhost:8080';
+export const API_ENDPOINT="https://rally-hobbyist.herokuapp.com/";
 
 export const TOKEN_KEY = 'rally-client-auth-token';
